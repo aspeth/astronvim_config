fb35401258a52f2cf10bf21fd55850834fdb90f4
Andrew Speth <andrew.speth@locushealth.com>
2023-08-04 14:32:38 -0600

    add rainbow indentation

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,5 +14,15 @@ return {
     cmd = "ASToggle",
     event = { "InsertLeave", "TextChanged" },
     opts = {},
+  },
+  {
+	"lukas-reineke/indent-blankline.nvim",
+	  opts = function(_, opts)
+		-- Other blankline configuration here
+		  return require("indent-rainbowline").make_opts(opts)
+	  end,
+	  dependencies = {
+		  "TheGLander/indent-rainbowline.nvim",
+	  },
   }
 }
