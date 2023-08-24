76315c1493f8f0197d778bf918472aeb9df1aee4
Andrew Speth <andrew.speth@locushealth.com>
2023-08-24 15:02:27 -0600

    turn off relative numbering, add ruby and yaml to treesitter

--- plugins/user.lua
+++ plugins/user.lua
@@ -16,7 +16,7 @@ return {
     opts = {},
   },
   {
-	"lukas-reineke/indent-blankline.nvim",
+	  "lukas-reineke/indent-blankline.nvim",
 	  opts = function(_, opts)
 		-- Other blankline configuration here
 		  return require("indent-rainbowline").make_opts(opts)
